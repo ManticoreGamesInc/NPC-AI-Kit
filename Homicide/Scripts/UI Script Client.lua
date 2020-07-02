4cd191a7545e83b787cd17b540bf81a017022348
standardcombo <standardcombo@gmail.com>
2020-07-01 18:48:04 -0700

    Fixed the role text so it doesn't say "Text" while waiting for a team to be assigned.

--- Homicide/Scripts/UI Script Client.lua
+++ Homicide/Scripts/UI Script Client.lua
@@ -1,15 +1,23 @@
-local propRole = script:GetCustomProperty("Role"):WaitForObject()
+local ROLE_TEXT = script:GetCustomProperty("Role"):WaitForObject()
 local player = Game.GetLocalPlayer()
 
 function Tick(deltaTime)
     if player.isDead then
-        propRole.text = 'Spectator'
-        propRole:SetColor(Color.YELLOW)
+        ROLE_TEXT.text = 'Spectator'
+        ROLE_TEXT:SetColor(Color.YELLOW)
+        ROLE_TEXT.visibility = Visibility.INHERIT
+        
     elseif player.team == 1 and not player.isDead then
-        propRole.text = 'Bystander'
-        propRole:SetColor(Color.BLUE) 
+        ROLE_TEXT.text = 'Bystander'
+        ROLE_TEXT:SetColor(Color.BLUE)
+        ROLE_TEXT.visibility = Visibility.INHERIT
+        
     elseif player.team == 2 then
-        propRole.text = 'Murderer'
-        propRole:SetColor(Color.RED)
+        ROLE_TEXT.text = 'Murderer'
+        ROLE_TEXT:SetColor(Color.RED)
+        ROLE_TEXT.visibility = Visibility.INHERIT
+        
+    else
+    	ROLE_TEXT.visibility = Visibility.FORCE_OFF
     end
 end
